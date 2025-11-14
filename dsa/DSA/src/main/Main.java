41494bf7de5392e768de7fbd2e87c192c4cf71da
Shubhu1134 <shubhu1134@gmail.com>
2025-11-15 00:00:43 +0530

    adding some old project   work

---
+++ dsa/DSA/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        LinkedList nums =new LinkedList();
+//        nums.add(5);
+//        nums.add(9);
+//
+//        System.out.println(nums);
+//
+//        nums.addFirst(6);
+//
+//        System.out.println(nums);
+//        nums.addLast(8);
+//
+//        System.out.println(nums);
+
+    }
+}
