9397a4bd07044fd9f9dab1b175f6443760ae7e11
Shubhu1134 <shubhu1134@gmail.com>
2025-12-16 16:44:48 +0530

    added older crud project based on jdbc

---
+++ jdbc/firstJdbc/src/main/java/com/info/firstJdbc/Getconnection.java
@@ -0,0 +1,6 @@
+package com.info.firstJdbc;
+
+public class Getconnection {
+
+	private static Connection con = null;
+}
---
+++ jdbc/firstJdbc/src/main/java/com/info/firstJdbc/TestSelect.java
@@ -0,0 +1,5 @@
+package com.info.firstJdbc;
+
+public class TestSelect {
+
+}
